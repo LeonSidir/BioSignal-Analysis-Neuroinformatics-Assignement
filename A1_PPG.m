27e0b7bff5a7f8eb73bca03dbe07779515574f91
LeonSidir <149479019+LeonSidir@users.noreply.github.com>
2025-12-17 22:38:21 +0200

    A1_PPG.m

--- A1_PPG.m
+++ A1_PPG.m
@@ -19,17 +19,17 @@ P2 = abs(Y/L);P1 = P2(1:round(L/2+1));
 P1(2:end-1) = 2*P1(2:end-1);
 %Define the frequency domain f and plot the single-sided amplitude spectrum P1.
 f = Fs*(0:round(L/2))/L;
-subplot(3,2,3),plot(f,P1),title('Single-Sided Amplitude Spectrum'),xlabel('f (Hz)'),ylabel('|P(f)|'),xlim([0 1])
+subplot(3,2,3),plot(f,10*log10(P1)),title('Single-Sided Amplitude Spectrum'),xlabel('f (Hz)'),ylabel('|P(f)|'),xlim([0 1])
 
 % frequency-domain representation via periodogram
 [pxx,faxis] = periodogram(ppg,hamming(L),L,Fs,'psd') ;
-subplot(3,2,4),plot(faxis,pxx),title('periodogram'),xlabel('f (Hz)'),ylabel('PSD'),xlim([0 1])
+subplot(3,2,4),plot(faxis,10*log10(pxx)),title('periodogram'),xlabel('f (Hz)'),ylabel('PSD'),xlim([0 1])
 
 % WELCH-technique for estimating PSD
 WINDOW=256;NOVERLAP=32;NFFT=256;[pxx,faxis] = pwelch(ppg,WINDOW,NOVERLAP,NFFT,Fs,'onesided');
-subplot(3,2,5),plot(faxis,pxx),xlabel('f (Hz)'),ylabel('PSD'),xlim([0 10]),title('Welch-based PSD'),legend('32samples-overlap ')
+subplot(3,2,5),plot(faxis,10*log10(pxx)),xlabel('f (Hz)'),ylabel('PSD'),xlim([0 10]),title('Welch-based PSD'),legend('32samples-overlap ')
 
 WINDOW=256;NOVERLAP=200;NFFT=256;[pxx,faxis] = pwelch(ppg,WINDOW,NOVERLAP,NFFT,Fs,'onesided');
-subplot(3,2,6),plot(faxis,pxx),xlabel('f (Hz)'),ylabel('PSD'),xlim([0 10]),title('Welch-based PSD'),legend('200samples-overlap')
+subplot(3,2,6),plot(faxis,10*log10(pxx)),xlabel('f (Hz)'),ylabel('PSD'),xlim([0 10]),title('Welch-based PSD'),legend('200samples-overlap')
 
 
